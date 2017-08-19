277f66a1e0ee368a8c2d9f7ac7dbb7ef34cb21bf
Erwin Alberto <ealberto@me.com>
2017-08-19 08:32:26 -0400

    invoke createBuildJob

--- pipeline/job.groovy
+++ pipeline/job.groovy
@@ -4,6 +4,12 @@ slurper.classLoader = this.class.classLoader
 def global = slurper.parse(readFileFromWorkspace('pipeline/global_param.dsl'))
 def config = slurper.parse(readFileFromWorkspace('pipeline/services.dsl'))
 
+// create job for every service
+config.services.each { name, data ->
+  createBuildJob(data, global)
+}
+
+
 def createBuildJob(data, global_config){
 	pipelineJob("${data.job_name}"){
 	    definition {
