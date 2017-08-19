6fa4a1f4ac60ab72a505f826f740209596105ec5
Erwin Alberto <ealberto@me.com>
2017-08-19 08:20:32 -0400

    first commit

---
+++ pipeline/job.groovy
@@ -0,0 +1,26 @@
+def slurper = new ConfigSlurper()
+
+slurper.classLoader = this.class.classLoader
+def global = slurper.parse(readFileFromWorkspace('pipeline/global_param.dsl'))
+def config = slurper.parse(readFileFromWorkspace('pipeline/services.dsl'))
+
+def createBuildJob(data, global_config){
+	pipelineJob("${data.job_name}"){
+	    definition {
+	        cpsScm {
+	            scm {
+	              git{
+	                remote{
+	                  url(data.git_url)
+		              credentials(global_config.github_credential_id)
+	                }
+			        data.branches.each{
+			          branch("*/${it.key}")
+			        }
+	              }                
+	            }
+	          	scriptPath(data.jenkins_dsl_file)
+	        }
+	    }  
+	}
+}
---
+++ pipeline/services.dsl
@@ -0,0 +1,8 @@
+college_app_tracker_services{
+	college_app_tracker {
+		job_name = 'college-app-tracker'
+		git_url = 'https://github.com/erwindev/college-app-tracker.git'	
+	    branches=['master':'prod']		
+	    jenkins_dsl_file='Jenkinsfile'	
+	}
+}
